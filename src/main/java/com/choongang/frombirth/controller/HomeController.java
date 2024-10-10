2de58a38280d3448633debad1e4cb4a801231476
Yewon <qjqmti0113@gmail.com>
2024-10-10 21:49:14 +0900

    1010 cho 기본 css 작성

--- src/main/java/com/choongang/frombirth/controller/HomeController.java
+++ src/main/java/com/choongang/frombirth/controller/HomeController.java
@@ -11,17 +11,17 @@ public class HomeController {
     @GetMapping("/login")
     public String login() {
         return "common/login";
-
     }
+
     @GetMapping("/loginTest")
     public String loginTest() {
         return "common/loginTest";
     }
+
     @GetMapping("/signup")
     public String signup() {
         return "common/signup";
     }
-
 
 }
 
---
+++ src/main/react/users/login.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import '../../resources/static/css/common.css';
+
+function Login() {
+
+    return (
+        <div className="login-box">test</div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Login />
+);
